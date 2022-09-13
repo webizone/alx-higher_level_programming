bb3d150e95546c87c2c7737c630966da17b59832
webizone <webizone30@gmail.com>
2022-09-12 17:30:05 -0700

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-     c = 0
+     index = 0
      for i in range(x):
     try:
-    
+        while index < x:
             print(my_list[index], end="")
-            c += 1
+            index += 1
             print()
     except IndexError:
-        break
+        
         print()
-        return c
+        return (index)
