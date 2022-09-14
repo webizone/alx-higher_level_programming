3feef56098dc141d67646f0bae7b834be490d1cb
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2022-09-14 02:44:11 +0100

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Create a Class Square with size, method of area and getters & setters"""
 
+
 class Square:
     """Class - Square"""
 
@@ -13,21 +14,21 @@ class Square:
         else:
             self.__size = size
 
-            def area(self):
-                """Method to get the area of the Square"""
-                return (self.__size ** 2)
+    def area(self):
+        """Method to get the area of the Square"""
+        return (self.__size ** 2)
 
-            @property
-            def size(self):
-                """Getter of the private attribute size"""
-                return (self.__size)
+    @property
+    def size(self):
+        """Getter of the private attribute size"""
+        return (self.__size)
 
-            @size.setter
-            def size(self, value):
-                """Setter for the size private attribute"""
-                if (type(value) is not int):
-                    raise (TypeError("size must be an integer"))
-                elif (value < 0):
-                    raise (ValueError("size must be >= 0"))
-                else:
-                    self.__size = value
+    @size.setter
+    def size(self, value):
+        """Setter for the size private attribute"""
+        if (type(value) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (value < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = value
