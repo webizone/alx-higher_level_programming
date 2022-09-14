e63559e94e992efe64d4b1ca8dac611f2e5b675f
webizone <webizone30@gmail.com>
2022-09-13 18:27:20 -0700

    101

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+class Square:
+
+        """Represents a square
+
+
+
+            Attributes:
+
+                    __size (int): size of a size of the square
+
+                            __position (tuple): position of the square in 2D space
+
+                                """
+
+def __init__(self, size=0, position=(0, 0)):
+
+                                                """initializes the square
+
+
