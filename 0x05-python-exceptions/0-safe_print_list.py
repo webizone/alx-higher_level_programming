1c64459b55fb700937d5e3183e41049a4730c04a
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2022-09-15 01:26:46 +0100

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-     index = 0
-     for i in range(x):
-    try:
-        while index < x:
-            print(my_list[index], end="")
-            index += 1
-            print()
-    except IndexError:
-        
-        print()
-        return (index)
+    c = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            c += 1
+        except IndexError:
+            break
+    print()
+    return c
