35e422b4336f72e28fe57586261188179e534411
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2022-09-28 00:33:47 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,10 @@
-2
+#!/usr/bin/python3
+"""
+Contains function that appends to text file and returns num chars added
+"""
+
+
+def append_write(filename="", text=""):
+    """appends to text file and returns num chars added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return(f.write(text))
